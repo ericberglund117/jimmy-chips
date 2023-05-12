b39cab27aa286f01d7ad3f2deb0ad6ebb0b3c21a
Eric Berglund <ericberglund117@gmail.com>
2023-05-11 19:09:11 -0600

    Install react router dom 6

---
+++ jimmy-chips/src/components/homepage/Homepage.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './Homepage.css';
+
+const Homepage = () => {
+  return (
+    <div className="App">
+        
+    </div>
+  );
+}
+
+export default Homepage;
