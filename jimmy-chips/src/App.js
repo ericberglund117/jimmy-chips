8de0aa0250f16aeb9b8a7d6f746aa618fb1ed058
Eric Berglund <ericberglund117@gmail.com>
2023-05-15 17:26:31 -0600

    Toggle instructions display

--- jimmy-chips/src/App.js
+++ jimmy-chips/src/App.js
@@ -6,8 +6,9 @@ import  Homepage  from './components/homepage/homepage'
 function App() {
   return (
     <div className="App">
+      <h1 className="title">Jimmy Chips</h1>
       <section className="navContainer">
-
+        
       </section>
       <Routes>
           <Route path="/" element= { <Homepage /> } />
---
+++ jimmy-chips/src/pages/setup/setup.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import './setup.css';
+
+const Setup = () => {
+    const instructions = "Jimmy Chips!"
+
+  return (
+    <div className="App">
+     <p>play bitch</p>
+    </div>
+  );
+}
+
+export default Setup;
