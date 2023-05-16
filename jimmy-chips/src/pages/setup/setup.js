316684f593e18439444c14268b7a13f553c6a722
Eric Berglund <ericberglund117@gmail.com>
2023-05-16 10:50:27 -0600

    Display names after input

--- jimmy-chips/src/pages/setup/setup.js
+++ jimmy-chips/src/pages/setup/setup.js
@@ -1,11 +1,41 @@
-import { Route, Routes } from 'react-router-dom';
+
+import { useState } from 'react';
 import './setup.css';
 
 const Setup = () => {
+    const [value, setValue] = useState('')
+    const [playerNames, setPlayerNames] = useState([''])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (playerNames.length === 0) {
+            setPlayerNames(value)
+        } else {
+            setPlayerNames([...playerNames, value])
+        }
+    }
+
+    const displayPlayers = playerNames.length > 0 ? playerNames.map((player, index) => <p className='addedPlayer' key={index}>{player}</p>) : null
 
   return (
     <div className="setup">
-     <p>play bitch</p>
+        <p className='playersInput'>Add players here:</p>
+     <form className='playerForm'>
+        <div className='form-control'>
+            <label htmlFor='playerName'>Name: </label>
+            <input 
+                type='text'
+                id='playerName'
+                name='playerName'
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                />
+        </div>
+        <button className='submit' type='submit' onClick={handleSubmit}>Submit</button>
+     </form>
+     <div className='playersContainer'>
+        {displayPlayers}
+     </div>
     </div>
   );
 }
