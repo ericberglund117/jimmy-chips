a1b77557f2d9715edd0988ccd4fc734d378dee17
Eric Berglund <ericberglund117@gmail.com>
2023-07-21 14:05:30 -0600

    Add screenshots to assets

--- jimmy-chips/src/components/homepage/Homepage.js
+++ jimmy-chips/src/components/homepage/Homepage.js
@@ -5,7 +5,7 @@ import './homepage.css';
 const Homepage = () => {
     const [showInstructions, setShowInstructions] = useState(false);
     const navigate = useNavigate()
-    const instructions = "Each player begins the round with no chips.\nThe first player that hits a shot that corresponds to the designation on the chip is given that chip. He/she holds on to that chip until someone else hits the same shot.\nFor Example, if player number 1 goes into the sand he/she will hold that chip until another player goes into the sand. He/she then gives the chip to that player. The same holds true for the positive chips. The last one to get a one putt or a birdie is the one that gets that chip.\nAt the beginning of the round, each chip is given a designated dollar value. At the end of the round those players holding the negative chips will pay to each player the designated amount for each chip he or she is holding.\nThose holding the positive chips will receive the same amount from each of the other players."
+    const instructions = "Each player begins the round with no chips. The first player that hits a shot that corresponds to the designation on the chip is given that chip. He/she holds on to that chip until someone else hits the same shot. For Example, if player number 1 goes into the sand he/she will hold that chip until another player goes into the sand. He/she then gives the chip to that player. The same holds true for the positive chips. The last one to get a one putt or a birdie is the one that gets that chip. At the beginning of the round, each chip is given a designated dollar value. At the end of the round those players holding the negative chips will pay to each player the designated amount for each chip he or she is holding. Those holding the positive chips will receive the same amount from each of the other players."
     
     const startPlay = () => {
         navigate("/setup")
@@ -18,17 +18,12 @@ const Homepage = () => {
         </section>
         <section className='homepageButtons'>
             <button className="howToPlayButton" onClick={() => setShowInstructions(!showInstructions)}>
-                {showInstructions === true ? "Hide Instructions" : "How To Play"}
+               <p>
+               {showInstructions === true ? "Hide Instructions" : "How To Play"}
+               </p> 
             </button>
             <button className="playButton" onClick={startPlay}> Play!</button>
-        </section>
-        {/* <section className='fillerContainer'>
-            <div className="wrapper">
-                <div className="ball"> 
-                    <div className="texture"></div>  
-                </div>
-            </div>
-        </section> */}
+        </section> 
     </div>
   );
 }
