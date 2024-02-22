a34a23bdee93b8e2e2bb9f52e9f36c4d3663f8ad
Eric Berglund <ericberglund117@gmail.com>
2024-02-21 17:10:38 -0700

    Edit express files

--- jimmy-chips/express/server/server_index.js
+++ jimmy-chips/express/server/server_index.js
@@ -1,8 +1,43 @@
 import express from "express";
+import bodyParser from 'body-parser';
+import { allChips, negChips, posChips } from "../../src/utils.js";
 
 const PORT = process.env.PORT || 3001;
-
 const serverApp = express();
+
+const singlePlayer = {
+    name: '',
+    chips: [],
+    score: {},
+    full: false,
+    holeCount: 1
+}
+const allPlayers = []
+
+// all players
+// single players
+// all chips
+// selected chips
+// 
+
+
+serverApp.use(bodyParser.json())
+
+serverApp.get('/api/allPlayers', (req, res) => {
+    res.json({ "allPlayers": allPlayers })
+})
+
+serverApp.post('/api/allPlayers', (req, res) => {
+    const newPlayer = req.body
+    allPlayers.push(newPlayer);
+    res.status(201).json(newPlayer)
+})
+
+serverApp.post(`/api/updatePlayer/${singlePlayer.name}`, (req, res) => {
+    const updatedPlayer = req.body
+    allPlayers.push(updatedPlayer);
+    res.status(201).json(updatedPlayer)
+})
 
 serverApp.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
