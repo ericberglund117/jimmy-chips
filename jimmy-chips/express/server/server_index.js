36054dadac565a50bbbb18411a7c54378b9a2db5
Eric Berglund <ericberglund117@gmail.com>
2024-02-25 18:43:45 -0700

    Add post to server_index

--- jimmy-chips/express/server/server_index.js
+++ jimmy-chips/express/server/server_index.js
@@ -18,24 +18,39 @@ const allPlayers = []
 // single players
 // all chips
 // selected chips
-// 
+    // create log in for:
+// game records
+    
+
+//side note with score
+    // have buttons/dropdown 1 - 10 for players to click instead of entering score like grint/18birdies
 
 
 serverApp.use(bodyParser.json())
 
+//get all players
 serverApp.get('/api/allPlayers', (req, res) => {
     res.json({ "allPlayers": allPlayers })
 })
-
+//get single player
+serverApp.get(`/api/${singlePlayer.name}`, (req, res) => {
+    const result =  allPlayers.find(player => player.name === singlePlayer.name)
+    res.json({ "singlePlayer": result })
+})
+//add player 
 serverApp.post('/api/allPlayers', (req, res) => {
     const newPlayer = req.body
     allPlayers.push(newPlayer);
     res.status(201).json(newPlayer)
 })
-
+//update single player info
 serverApp.post(`/api/updatePlayer/${singlePlayer.name}`, (req, res) => {
     const updatedPlayer = req.body
-    allPlayers.push(updatedPlayer);
+    const playerUpdate = allPlayers.find(player => player.name === singlePlayer.name)
+    let index = allPlayers.indexOf(singlePlayer)
+    if (~index) {
+        allPlayers[index] = playerUpdate
+    }
     res.status(201).json(updatedPlayer)
 })
 
